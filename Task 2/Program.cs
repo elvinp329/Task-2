f437364f9251d4f5e00212d84e0fe3d635a8013b
Elvinp329 <elvinkj@code.edu.az>
2022-12-28 01:48:01 +0400

    TAsk2

---
+++ Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin");
+
+
+            int i = int.Parse(Console.ReadLine());
+
+            int a = 1 * (i / 1000);
+            int b = 2 * ((i / 100) % 10);
+            int c = 3 * ((i / 10) % 10);
+            int d = 4 * (i % 10);
+
+            Console.WriteLine(a + b + c + d);
+
+            
+        }
+    }
+}
